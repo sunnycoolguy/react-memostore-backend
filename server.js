5d33eafc65f51fa85f213be92058b639a9adf144
senaybee <senay_berihu@outlook.com>
2020-06-24 19:40:36 -0700

    Connect app to a Mongo database

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+
+    console.log('Connected to the database');
+});
 
 app.use(bodyParser.urlencoded({ extended : true }));
 
