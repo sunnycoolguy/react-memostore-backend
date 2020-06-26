5d2c29e3f27f33a5902e7f724d18ef66db77b8ca
senaybee <senay_berihu@outlook.com>
2020-06-25 19:13:39 -0700

    Fix very annoying CORS bug

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+    if (err) return console.log(err);
+    const db = client.db('memo-store');
+    const usersCollection = db.collection('users'); 
 
-    console.log('Connected to the database');
+    app.use(cors());
+    
+    app.use(bodyParser.json());
+    
+    
+    app.post('/', function (req, res){
+        usersCollection.insertOne(req.body)
+        .then(res.send('hello!')) 
+        .catch(err => console.log(error));
+    })
+
+
+    app.get('/:username', function (req, res){
+        usersCollection.findOne({username : req.params.username})
+        .then((result) => { res.send(result) })
+        .catch(console.log(err));
+    })
+
+    app.listen(4001, () => {
+        console.log('I am listening!');
+    })
+
 });
-
-app.use(bodyParser.urlencoded({ extended : true }));
-
-
-app.post('/', function (req, res){
-    console.log(req.body);
-    res.send('wow');
-})
-
-
-app.get('/', function (req, res){
-    res.send('Hey I\'m here for the interview!');
-})
-
-app.listen(4001, () => {
-    console.log('I am listening!');
-})
-
-
 
 /*
 var mongo = require('mongodb');
